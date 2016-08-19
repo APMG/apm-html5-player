99f3aabf042fcc93003f4af475d44b48cd6962a5
Hankerson, Geoffrey <ghankerson@mpr.org>
2016-08-19 10:15:33 -0500

    intial committ

---
+++ script/FormatTime.js
@@ -0,0 +1,58 @@
+define(function(require){
+  'use strict';
+
+  // Constructor
+  var FormatTime = function() {};
+
+  // Formats an ugly time (in seconds) to a nice readable format
+  // e.g. 125 > 2:05, or 4226 > 1:10:26
+  FormatTime.prototype.toFormatted = function(timeInSeconds) {
+    timeInSeconds = Math.round(timeInSeconds);
+
+    var formattedTime = '';
+    var formattedMinutes = '';
+    var formattedSeconds = '';
+    var hours = Math.floor(timeInSeconds / 3600);
+    var minutes = Math.floor((timeInSeconds / 60) - (hours * 60));
+    var seconds = timeInSeconds - (minutes * 60) - (hours * 3600);
+
+    if (hours !== 0) {
+      formattedTime = hours + ':';
+
+      if (minutes < 10) {
+        formattedMinutes = '0' + minutes;
+      } else {
+        formattedMinutes = minutes.toString();
+      }
+    } else {
+      formattedMinutes = minutes.toString();
+    }
+
+    if (seconds < 10) {
+      formattedSeconds = '0' + seconds;
+    } else {
+      formattedSeconds = seconds.toString();
+    }
+
+    formattedTime = formattedTime + formattedMinutes + ':' + formattedSeconds;
+
+    return formattedTime;
+  };
+
+  // Removes formatting from a timestamp, converting it to seconds
+  // e.g. 2:05 > 125, or 1:10:26 > 4226
+  FormatTime.prototype.toSeconds = function(formattedTime) {
+    var arrHms = formattedTime.split(':');
+    var arrSmh = arrHms.reverse();
+    var timeInSeconds = 0;
+    var seconds = parseInt(arrSmh[0] || 0);
+    var minutes = parseInt(arrSmh[1] * 60 || 0);
+    var hours = parseInt(arrSmh[2] * 3600 || 0);
+
+    timeInSeconds = hours + minutes + seconds;
+
+    return timeInSeconds;
+  };
+
+  return FormatTime;
+});
---
+++ script/Playlist.js
@@ -0,0 +1,119 @@
+// Required modules for page load
+define(function(require, exports, module) { // eslint-disable-line
+  'use strict';
+
+  // Require external modules
+  var $ = require('jquery');
+
+  // Set up reference to parent module.
+  var player;
+
+  // Constructor
+  // The 'parent' argument passed in is the parent object from Player.js.
+  // This script is only intended to be used with that Player.js.
+  var Playlist = function(parent) {
+    player = parent;
+    // The containing DOM element
+    this.$el = player.$playlistElement;
+  };
+
+  // -----------------------------
+  // Setup functions
+  // -----------------------------
+
+  Playlist.prototype.init = function() {
+    this.selectElements()
+        .setNextItem()
+        .bindEventHandlers();
+  };
+
+  Playlist.prototype.selectElements = function() {
+    this.$items = this.$el.find('.js-playlist-item');
+
+    return this;
+  };
+
+  Playlist.prototype.setNextItem = function() {
+    this.$items.each(function() {
+      var $currentItem = $(this);
+      var $nextItem = $(this).next();
+      var nextSrc;
+
+      if ($nextItem.length === 0) { return; }
+
+      nextSrc = $nextItem.data('src');
+      $currentItem.attr('data-next', nextSrc);
+    });
+
+    return this;
+  };
+
+  Playlist.prototype.bindEventHandlers = function() {
+    this.$items.on('click', this.onItemClick.bind(this));
+
+    return this;
+  };
+
+  // -----------------------------
+  // Event Handlers
+  // -----------------------------
+
+  Playlist.prototype.onItemClick = function(e) {
+    e.preventDefault();
+    var $target = $(e.currentTarget);
+    var src = $target.data('src');
+    var title = $target.data('title');
+    var artist = $target.data('artist');
+
+    player.loadAudioFromSrc(src);
+    player.playAudio();
+    this.displayPlayedState($target);
+    this.displayBufferingState($target);
+    this.populatePlayerInfo(title, artist);
+  };
+
+  // -----------------------------
+  // Helpers
+  // -----------------------------
+
+  Playlist.prototype.displayPlayedState = function($item) {
+    this.removeDisplayStates();
+    $item.addClass('is-playing');
+  };
+
+  Playlist.prototype.displayPlayingState = function($item) {
+    this.removeBufferingState($item);
+    this.removePausedState($item);
+  };
+
+  Playlist.prototype.displayPausedState = function($item) {
+    $item.addClass('is-paused');
+  };
+
+  Playlist.prototype.removePausedState = function($item) {
+    $item.removeClass('is-paused');
+  };
+
+  Playlist.prototype.displayBufferingState = function($item) {
+    $item.addClass('is-loading');
+  };
+
+  Playlist.prototype.removeBufferingState = function($item) {
+    $item.removeClass('is-loading');
+  };
+
+  Playlist.prototype.removeDisplayStates = function() {
+    this.removeBufferingState(this.$items);
+    this.$items.removeClass('is-active');
+    this.$items.removeClass('is-paused');
+    this.$items.removeClass('is-playing');
+  };
+
+  Playlist.prototype.populatePlayerInfo = function(title, artist) {
+    player.$title.text(title);
+    player.$artist.text(artist);
+  };
+
+  return Playlist;
+
+});
