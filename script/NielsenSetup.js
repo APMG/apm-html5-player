09a0f81a6f0c3558181a5d10edc4fad8f9bd63f3
Hankerson, Geoffrey <ghankerson@mpr.org>
2016-09-27 15:07:31 -0500

    remove nil check that prevents executions on nielsen

--- script/NielsenSetup.js
+++ script/NielsenSetup.js
@@ -5,9 +5,6 @@ define(['nielsen'], function() {
   };
 
   NielsenSetup.prototype.init = function(params, metadata) {
-    if(typeof(window.NOLCMB === 'undefined')) {
-      return;
-    }
     window.nielsenMetadataObject = metadata;
     window.nSdkInstance = window.NOLCMB.getInstance(params);
     window.nSdkInstance.ggInitialize(params);
